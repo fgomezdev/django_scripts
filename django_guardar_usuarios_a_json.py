ca4f026fea7d8b78a2adb7d6789fe39d0d0e20f1
Fernando D. Gomez <fgomezdev@gmail.com>
2022-03-21 16:41:21 -0300

    Se elimina una línea repetida

--- django_guardar_usuarios_a_json.py
+++ django_guardar_usuarios_a_json.py
@@ -3,16 +3,15 @@ from django.contrib.auth.models import User
 
 usuarios = User.objects.all()
 lst_usuarios = []
-for user in usuarios: 
+for user in usuarios:
     if user.username in ["super_admin", "AnonymousUser"]:
         continue
 
-    user_data=user.__dict__ 
-    user_data.pop("_state", None)     
-    user_data.pop("last_login", None) 
-    user_data.pop("date_joined", None) 
-    user_data.pop("last_login", None) 
-    lst_usuarios.append(user_data) 
+    user_data=user.__dict__
+    user_data.pop("_state", None)
+    user_data.pop("last_login", None)
+    user_data.pop("date_joined", None)
+    lst_usuarios.append(user_data)
 
 with open("usuarios.json", "w") as resultado: 
     json.dump(lst_usuarios, resultado)
