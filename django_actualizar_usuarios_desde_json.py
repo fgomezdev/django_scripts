c35de611ec34e29b894b6b3526e79fbb02ca5f00
Fernando D. Gómez <fgomezdev@gmail.com>
2024-12-09 14:04:26 -0300

    se agrega la exportación/importación de grupos y permisos

--- django_actualizar_usuarios_desde_json.py
+++ django_actualizar_usuarios_desde_json.py
@@ -1,41 +1,127 @@
 import json
-from django.contrib.auth.models import User
 
-actualizar_si_existe = False  #OJO!
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 
-try:
-    with open("usuarios.json") as json_file:
-        json_data = json.load(json_file)
+User = get_user_model()
 
-    for data in json_data:
-        user_to_create_or_update = None
+actualizar_si_existe = True  # OJO!
+usuarios_a_excluir = ["admin", "super_admin", "AnonymousUser"]
+grupos_a_excluir = []
+permisos_a_excluir = []
 
-        try:
-            user_to_create_or_update = User.objects.get(username=data['username'])            
-        except:
-            if actualizar_si_existe:
-                print(f"El usuario {data['username']} no existe!")
-                
-        if user_to_create_or_update is not None:
-            if actualizar_si_existe:
-                print(f"Actualizando usuario: {data['username']}")
-                user_to_create_or_update.is_superuser=data['is_superuser']
-                user_to_create_or_update.first_name=data['first_name']            
-                user_to_create_or_update.last_name=data['last_name']
-                user_to_create_or_update.email=data['email']
-                user_to_create_or_update.is_staff=data['is_staff']
-                user_to_create_or_update.is_active=data['is_active']
-                # En caso de querer guardar la contraseña
-                # no guardar así -> user_to_create_or_update.password=data['password']
-                # porque no se guarda como hash, sino como texto plano
-                # así sí: -> user_to_create_or_update.set_password(data['password'])                
+SET_PASSWORDS = True
+
+
+def importar_usuarios():
+    try:
+        with open("usuarios.json") as json_file:
+            json_data = json.load(json_file)
+
+        for data in json_data:
+            if data["username"] in usuarios_a_excluir:
+                print(f"Saltando usuario: {data['username']}")
+                continue
+
+            user_to_create_or_update = None
+
+            try:
+                user_to_create_or_update = User.objects.get(username=data["username"])
+            except:
+                if actualizar_si_existe:
+                    print(f"El usuario {data['username']} no existe!")
+
+            user_permissions = Permission.objects.filter(
+                codename__in=data["user_permissions"]
+            ).values_list("id", flat=True)
+            if user_to_create_or_update is not None:
+                if actualizar_si_existe:
+                    print(f"Actualizando usuario: {data['username']}")
+                    user_to_create_or_update.is_superuser = data["is_superuser"]
+                    user_to_create_or_update.first_name = data["first_name"]
+                    user_to_create_or_update.last_name = data["last_name"]
+                    user_to_create_or_update.email = data["email"]
+                    user_to_create_or_update.is_staff = data["is_staff"]
+                    user_to_create_or_update.is_active = data["is_active"]
+                else:
+                    print(f"El usuario {data['username']} existe, saltando...")
             else:
-                print(f"El usuario {data['username']} existe, saltando...")
-        else:
-            print(f"Creando usuario: {data['username']}")
-            user_to_create_or_update = User.objects.create(is_superuser=data['is_superuser'],first_name=data['first_name'],username=data['username'],last_name=data['last_name'],email=data['email'],is_staff=data['is_staff'],is_active=data['is_active'])
-            
-        if user_to_create_or_update is not None:
-            user_to_create_or_update.save()
-except Exception as ex:
-    print(f"Error al crear usuarios: {ex}")
+                print(f"Creando usuario: {data['username']}")
+                user_to_create_or_update = User.objects.create(
+                    is_superuser=data["is_superuser"],
+                    first_name=data["first_name"],
+                    username=data["username"],
+                    last_name=data["last_name"],
+                    email=data["email"],
+                    is_staff=data["is_staff"],
+                    is_active=data["is_active"],
+                )
+
+            user_to_create_or_update.groups.clear()
+            user_to_create_or_update.user_permissions.clear()
+
+            groups = Group.objects.filter(name__in=data["groups"])
+            user_to_create_or_update.groups.set(groups)
+
+            user_permissions = Permission.objects.filter(
+                codename__in=data["user_permissions"]
+            )
+            user_to_create_or_update.user_permissions.set(user_permissions)
+
+            # En caso de querer guardar la contraseña
+            # no guardar así -> user_to_create_or_update.password=data['password']
+            # porque no se guarda como hash, sino como texto plano
+            # así sí: -> user_to_create_or_update.set_password(data['password'])
+            if SET_PASSWORDS:
+                user_to_create_or_update.set_password(data["password"])
+
+            if user_to_create_or_update is not None:
+                user_to_create_or_update.save()
+    except Exception as ex:
+        print(f"Error al crear usuarios: {ex}")
+
+
+def importar_grupos():
+    try:
+        with open("grupos.json") as json_file:
+            json_data = json.load(json_file)
+
+        for data in json_data:
+            if data["name"] in grupos_a_excluir:
+                print(f"Saltando grupo: {data['name']}")
+                continue
+
+            group_to_create_or_update = None
+
+            try:
+                group_to_create_or_update = Group.objects.get(name=data["name"])
+            except:
+                if actualizar_si_existe:
+                    print(f"El grupo {data['name']} no existe!")
+
+            permissions = Permission.objects.filter(
+                codename__in=data["permissions"]
+            ).values_list("id", flat=True)
+            if group_to_create_or_update is not None:
+                if actualizar_si_existe:
+                    print(f"Actualizando grupo: {data['name']}")
+                else:
+                    print(f"El grupo {data['name']} existe, saltando...")
+            else:
+                print(f"Creando grupo: {data['name']}")
+                group_to_create_or_update = Group.objects.create(
+                    name=data["name"],
+                )
+
+            group_to_create_or_update.permissions.clear()
+            permissions = Permission.objects.filter(codename__in=data["permissions"])
+            group_to_create_or_update.permissions.set(permissions)
+
+            if group_to_create_or_update is not None:
+                group_to_create_or_update.save()
+    except Exception as ex:
+        print(f"Error al crear grupos: {ex}")
+
+
+importar_grupos()
+importar_usuarios()
--- django_guardar_usuarios_a_json.py
+++ django_guardar_usuarios_a_json.py
@@ -1,17 +1,62 @@
 import json
-from django.contrib.auth.models import User
 
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+User = get_user_model()
+
+# Usuarios
 usuarios = User.objects.all()
+
+print(f"Usuarios a exportar: {len(usuarios)}")
 lst_usuarios = []
 for user in usuarios:
-    if user.username in ["super_admin", "AnonymousUser"]:
+    if user.username in ["admin", "super_admin", "AnonymousUser"]:
         continue
 
-    user_data=user.__dict__
+    user_data = user.__dict__
+    user_data["groups"] = list(user.groups.values_list("name", flat=True))
+    user_data["user_permissions"] = list(
+        user.user_permissions.values_list("codename", flat=True)
+    )
+
     user_data.pop("_state", None)
     user_data.pop("last_login", None)
     user_data.pop("date_joined", None)
     lst_usuarios.append(user_data)
 
-with open("usuarios.json", "w") as resultado: 
+with open("usuarios.json", "w") as resultado:
     json.dump(lst_usuarios, resultado)
+
+
+# Grupos
+groups = Group.objects.all()
+print(f"Grupos a exportar: {len(groups)}")
+lst_grupos = []
+for group in groups:
+    group_data = group.__dict__
+    group_data["permissions"] = list(
+        group.permissions.values_list("codename", flat=True)
+    )
+
+    group_data.pop("_state", None)
+    group_data.pop("last_login", None)
+    group_data.pop("date_joined", None)
+    lst_grupos.append(group_data)
+
+with open("grupos.json", "w") as resultado:
+    json.dump(lst_grupos, resultado)
+
+# # Permisos
+# permissions = Permission.objects.all()
+# print(f"Permisos a exportar: {len(permissions)}")
+# lst_permisos = []
+# for permission in permissions:
+#     permission_data = permission.__dict__
+#     permission_data.pop("_state", None)
+#     permission_data.pop("last_login", None)
+#     permission_data.pop("date_joined", None)
+#     lst_permisos.append(permission_data)
+
+# with open("permisos.json", "w") as resultado:
+#     json.dump(lst_permisos, resultado)
